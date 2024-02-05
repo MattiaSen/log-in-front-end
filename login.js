2a90427d9db2a50eb7043dc2e27e0326208da0bc
MattiaSen <147874985+MattiaSen@users.noreply.github.com>
2024-02-05 14:30:10 +0100

    two

--- login.js
+++ login.js
@@ -12,3 +12,19 @@ document.getElementById("reg-log").addEventListener("click", function() {
         document.getElementById("reg-log").innerHTML = "Log-in";
     }
 });
+
+// Aggiungi un altro listener per l'evento "click" per l'opzione "Register"
+document.getElementById("reg-log").addEventListener("click", function() {
+    // Inverti la visibilità dell'input "Confirm password"
+    var confirmPasswordInput = document.getElementById("confirmpass");
+    confirmPasswordInput.style.display = (confirmPasswordInput.style.display === "block") ? "none" : "block";
+
+    // Inverti il testo e l'azione dell'opzione "Register"
+    if (confirmPasswordInput.style.display === "none") {
+        document.getElementById("question").innerHTML = "You don't have an account?";
+        document.getElementById("reg-log").innerHTML = "Register";
+    } else {
+        document.getElementById("question").innerHTML = "You have an account?";
+        document.getElementById("reg-log").innerHTML = "Log-in";
+    }
+});
